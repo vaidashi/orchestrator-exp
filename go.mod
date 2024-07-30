f5471072b41cf40b8dfc427db98ee43bad064df6
vaidashi <vaid50@yahoo.com>
2024-07-30 13:55:34 -0500

    creating, running, stopping, removing tasks in place

--- go.mod
+++ go.mod
@@ -3,12 +3,28 @@ module orchestrator-exp
 go 1.17
 
 require (
+	github.com/docker/docker v20.10.6+incompatible
 	github.com/docker/go-connections v0.4.0
+	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
 	github.com/google/uuid v1.2.0
+	github.com/moby/moby v20.10.6+incompatible
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
-	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/Microsoft/go-winio v0.5.0 // indirect
+	github.com/containerd/containerd v1.5.1 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	google.golang.org/genproto v0.0.0-20201110150050-8816d57aaa9a // indirect
+	google.golang.org/grpc v1.37.1 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -8,10 +8,51 @@ import (
  
     "github.com/golang-collections/collections/queue"
     "github.com/google/uuid"
+	"github.com/docker/docker/client"
  
     "orchestrator-exp/manager"
     "orchestrator-exp/worker"
 )
+
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name:          "container1",
+		Image: "postgres:13",
+		Env: []string{
+			"POSTGRES_USER=bob",
+			"POSTGRES_PASSWORD=secret",
+		},
+	}
+
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+
+    d := task.Docker{
+        Client: dc,
+        Config: c,
+    }
+ 
+    result := d.Run()
+
+    if result.Error != nil {
+        fmt.Printf("%v\n", result.Error)
+        return nil, nil
+    }
+ 
+    fmt.Printf("Container %s is running with config %v\n", result.ContainerId, c)
+    return &d, &result
+}
+
+func stopContainer(d *task.Docker, id string) *task.DockerResult {
+    result := d.Stop(id)
+
+    if result.Error != nil {
+        fmt.Printf("%v\n", result.Error)
+        return nil
+    }
+ 
+    fmt.Printf("Container %s has been stopped and removed\n", result.ContainerId)
+    return &result
+}
 
 func main() {
 	t := task.Task{
@@ -67,4 +108,12 @@ func main() {
 	}
 
 	fmt.Printf("node: %v\n", n)
+
+	fmt.Printf("create a test container\n")
+	dockerTask, createResult := createContainer()
+
+	time.Sleep(time.Second * 5)
+
+	fmt.Printf("stopping container %s\n", createResult.ContainerId)
+	_ = stopContainer(dockerTask, createResult.ContainerId)
 }
